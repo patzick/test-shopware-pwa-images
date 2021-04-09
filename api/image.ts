21a1e040448113d5b350c06adec5bd62258d779e
patzick <13100280+patzick@users.noreply.github.com>
2021-04-09 14:15:37 +0200

    feat: default format to webp

--- api/image.ts
+++ api/image.ts
@@ -5,18 +5,20 @@ const fetch = require("node-fetch");
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const t0 = performance.now();
-  console.log("hello images", request.query, request.headers.accept);
+  console.log("input data", request.query, request.headers.accept);
 
-  const outputFormat = request.headers?.accept?.split(",")?.[0] || "image/jpeg";
-  const format = outputFormat.split("/")?.[1] || "jpeg";
+  const outputFormat =
+    request.headers?.accept?.split(",")?.find((el) => el.includes("image/")) ||
+    "image/jpeg";
+  let format = outputFormat.split("/")?.[1] || "jpeg";
 
   // image/webp,*/*
   // image/avif,image/webp,image/apng,image/*,*/*;q=0.8
 
   const preferredWith = parseInt(request.query.w as string) || 100;
   const preferredHeight = parseInt(request.query.h as string) || null;
-  console.log("hello dimentions", preferredWith, preferredHeight);
-  console.log("we should prepare for", outputFormat, format);
+  console.log("input dimentions", preferredWith, preferredHeight);
+  console.log("desired output format", outputFormat, format);
 
   // const { name = "World" } = request.query;
   // // console.error('is it a log??', request.query)
@@ -38,14 +40,16 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     });
     // .avif({ quality: 60, speed: 7 })
     switch (format) {
+      // result = await result.avif({ quality: 60, speed: 8 });
+      // break;
       case "avif":
-        result = await result.avif({ quality: 60, speed: 8 });
-        break;
       case "webp":
-        result = await result.webp({ quality: 60 });
+        result = await result.webp({ quality: 60, reductionEffort: 5 });
+        format = "webp";
         break;
       default:
         result = await result.jpeg({ quality: 70 });
+        format = "jpeg";
         break;
     }
     result = await result.toBuffer();
@@ -54,7 +58,7 @@ export default async (request: VercelRequest, response: VercelResponse) => {
 
     // response.send({ data: 'data' })
     const t2 = performance.now();
-    console.error(
+    console.log(
       `Image load: ${Math.round(t1 - t0)} ms; processing ${Math.round(
         t2 - t1
       )} ms; Whole endpoint ${Math.round(t2 - t0)} ms`
@@ -63,11 +67,13 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     // response.set("Cache-control", `public, max-age=${period}`);
     // response.contentType(outputFormat);
     // response.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
+    console.log("-> format", `image/${format}`);
     response.setHeader("Cache-Control", "max-age=86400, s-maxage=86400");
+    response.setHeader("Content-Type", `image/${format}`);
     response.end(result, "binary");
   } catch (e) {
     const tEnd = performance.now();
-    console.error(`sad error (${Math.round(tEnd - t0)} ms);`, e);
-    response.send(":(");
+    console.error(`Fatal error (${Math.round(tEnd - t0)} ms);`, e);
+    response.send("Could not process image.");
   }
 };
